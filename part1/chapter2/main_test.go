cf795c9201b28f3dd8d9b705ab98e07630fa6695
bryutus <bryutus@gmail.com>
2017-11-01 01:46:12 +0900

    :sparkles: part1のchapter1をコピー

---
+++ part1/chapter2/main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMultiplication(t *testing.T) {
+	five := newDollar(5)
+	five.times(2)
+
+	if got, want := five.Amount, 10; got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
