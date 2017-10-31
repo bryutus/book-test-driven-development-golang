143a04e84fa229d0db2365492387bfe657d84fae
bryutus <bryutus@gmail.com>
2017-11-01 02:01:22 +0900

    :sparkles: timesメソッドでDollar構造体を返すように修正

--- part1/chapter2/main.go
+++ part1/chapter2/main.go
@@ -4,8 +4,8 @@ type Dollar struct {
 	Amount int
 }
 
-func (d *Dollar) times(multiplier int) {
-	d.Amount *= multiplier
+func (d *Dollar) times(multiplier int) *Dollar {
+	return newDollar(d.Amount * multiplier)
 }
 
 func newDollar(amount int) *Dollar {
