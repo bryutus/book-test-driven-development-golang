671cc02833ab0da22374303eb6cafa6f14e172bc
bryutus <bryutus@gmail.com>
2017-11-01 02:00:22 +0900

    :green_heart: Dollar構造体の副作用のテストを追加

--- part1/chapter2/main_test.go
+++ part1/chapter2/main_test.go
@@ -6,9 +6,14 @@ import (
 
 func TestMultiplication(t *testing.T) {
 	five := newDollar(5)
-	five.times(2)
 
-	if got, want := five.Amount, 10; got != want {
+	product := five.times(2)
+	if got, want := product.Amount, 10; got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+
+	product = five.times(3)
+	if got, want := product.Amount, 15; got != want {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
