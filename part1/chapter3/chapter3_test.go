c007362e6db06ddf88473eed092d68a14ff6d08c
bryutus <bryutus@gmail.com>
2017-11-01 13:59:10 +0900

    :green_heart: 三角測量を行うためのテストを追加

--- part1/chapter3/chapter3_test.go
+++ part1/chapter3/chapter3_test.go
@@ -25,3 +25,23 @@ func TestMultiplication(t *testing.T) {
 		})
 	}
 }
+
+func TestEquality(t *testing.T) {
+
+	testCases := []struct {
+		desc string
+		in   int
+		want bool
+	}{
+		{"5ドルと5ドルが同じ価値であること", 5, true},
+		{"5ドルと6ドルが異なる価値であること", 6, false},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			if got := newDollar(5).equals(newDollar(test.in)); got != test.want {
+				t.Errorf("Doller.equals(): got %v want %v", got, test.want)
+			}
+		})
+	}
+}
