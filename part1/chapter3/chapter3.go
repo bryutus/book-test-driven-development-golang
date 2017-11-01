822640c497fcb15a74721aa2cb6f84d5b566ce41
bryutus <bryutus@gmail.com>
2017-11-02 01:02:28 +0900

    :tada: 等価性を比較できるように実装を行った

--- part1/chapter3/chapter3.go
+++ part1/chapter3/chapter3.go
@@ -8,6 +8,10 @@ func (d *Dollar) times(multiplier int) *Dollar {
 	return newDollar(d.Amount * multiplier)
 }
 
+func (d *Dollar) equals(dollar *Dollar) bool {
+	return d.Amount == dollar.Amount
+}
+
 func newDollar(amount int) *Dollar {
 	d := new(Dollar)
 	d.Amount = amount
