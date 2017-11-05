14a1a96acf0fde9478cc6a148bcc222bf7fd012e
bryutus <bryutus@gmail.com>
2017-11-05 18:57:19 +0900

    :+1: FrancとDollarを比較できるように修正
    構造体を初期化する時にcurrencyフィールドを持たせて比較時に使用

--- part1/chapter7/money.go
+++ part1/chapter7/money.go
@@ -11,14 +11,16 @@ type Money interface {
 	times(int) Money
 	equals(Money) bool
 	getAmount() int
+	getCurrency() Currency
 }
 
 type money struct {
-	amount int
+	amount   int
+	currency Currency
 }
 
 func newMoney(currency Currency, amount int) Money {
-	m := money{amount: amount}
+	m := money{amount: amount, currency: currency}
 
 	if currency == Franc {
 		return franc{m}
@@ -28,9 +30,13 @@ func newMoney(currency Currency, amount int) Money {
 }
 
 func (m money) equals(money Money) bool {
-	return m.amount == money.getAmount()
+	return m.amount == money.getAmount() && m.currency == money.getCurrency()
 }
 
 func (m money) getAmount() int {
 	return m.amount
+}
+
+func (m money) getCurrency() Currency {
+	return m.currency
 }
