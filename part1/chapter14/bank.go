843caee5a954b7e2fa88497d40a93d869a9e86c5
bryutus <bryutus@gmail.com>
2017-11-14 10:19:21 +0900

    :sparkles: コンパイルを通すようにBankにメソッドを定義

--- part1/chapter14/bank.go
+++ part1/chapter14/bank.go
@@ -2,6 +2,7 @@ package chapter14
 
 type Bank interface {
 	reduce(Expression, Currency) Money
+	addRate(Currency, Currency, int)
 }
 
 type bank struct {
@@ -14,3 +15,5 @@ func newBank() Bank {
 func (b bank) reduce(source Expression, to Currency) Money {
 	return source.reduce(to)
 }
+
+func (b bank) addRate(from Currency, to Currency, rate int) {}
