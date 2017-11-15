4fc9c9cb96baa2f8c776a5ad858d63285c80ac57
bryutus <bryutus@gmail.com>
2017-11-16 02:59:41 +0900

    :tada: 為替レートをBankの中に格納して参照できるようにした

--- part1/chapter14/bank.go
+++ part1/chapter14/bank.go
@@ -7,22 +7,25 @@ type Bank interface {
 }
 
 type bank struct {
+	rates map[Pair]int
 }
 
 func newBank() Bank {
-	return bank{}
+	return &bank{}
 }
 
-func (b bank) reduce(source Expression, to Currency) Money {
+func (b *bank) reduce(source Expression, to Currency) Money {
 	return source.reduce(b, to)
 }
 
-func (b bank) addRate(from Currency, to Currency, rate int) {}
+func (b *bank) addRate(from Currency, to Currency, rate int) {
+	p := newPair(from, to)
+	b.rates = map[Pair]int{p: rate}
+}
 
-func (b bank) rate(from Currency, to Currency) (rate int) {
-	rate = 1
-	if from == Franc && to == Dollar {
-		rate = 2
+func (b *bank) rate(from Currency, to Currency) int {
+	if from == to {
+		return 1
 	}
-	return
+	return b.rates[newPair(from, to)]
 }
