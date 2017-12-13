d6aea63f6e0405f2e8c5e2cea83f40baaf53bccf
bryutus <bryutus@gmail.com>
2017-12-14 01:46:37 +0900

    :green_heart: テストファイルと実装ファイルを分離

--- part2/chapter18/xunit_test.go
+++ part2/chapter18/xunit_test.go
@@ -1,35 +1,8 @@
 package chapter18
 
 import (
-	"reflect"
 	"testing"
 )
-
-type TestCase struct {
-	name string
-}
-
-func newTestCase(name string) *TestCase {
-	return &TestCase{name: name}
-}
-
-func (t *TestCase) run(w *WasRun) {
-	method := t.name
-	fv := reflect.ValueOf(w).MethodByName(method)
-	fv.Call(nil)
-}
-
-type WasRun struct {
-	wasRun bool
-}
-
-func newWasRun() *WasRun {
-	return &WasRun{wasRun: false}
-}
-
-func (w *WasRun) TestMethod() {
-	w.wasRun = true
-}
 
 func TestRunning(testing *testing.T) {
 	testcase := newTestCase("TestMethod")
