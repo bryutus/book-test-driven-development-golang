0edf964deb7401a64aa62ea510b6bf81773935f4
bryutus <bryutus@gmail.com>
2017-12-14 01:23:03 +0900

    :recycle: メソッドの位置を変更

--- part2/chapter18/xunit.go
+++ part2/chapter18/xunit.go
@@ -5,21 +5,18 @@ import (
 	"reflect"
 )
 
-func main() {
-	testcase := newTestCase("TestMethod")
-	test := newWasRun()
-
-	fmt.Println(test.wasRun)
-	testcase.run(test)
-	fmt.Println(test.wasRun)
-}
-
 type TestCase struct {
 	name string
 }
 
 func newTestCase(name string) *TestCase {
 	return &TestCase{name: name}
+}
+
+func (t *TestCase) run(w *WasRun) {
+	method := t.name
+	fv := reflect.ValueOf(w).MethodByName(method)
+	fv.Call(nil)
 }
 
 type WasRun struct {
@@ -30,12 +27,15 @@ func newWasRun() *WasRun {
 	return &WasRun{wasRun: false}
 }
 
-func (t *TestCase) run(w *WasRun) {
-	method := t.name
-	fv := reflect.ValueOf(w).MethodByName(method)
-	fv.Call(nil)
-}
-
 func (w *WasRun) TestMethod() {
 	w.wasRun = true
+}
+
+func main() {
+	testcase := newTestCase("TestMethod")
+	test := newWasRun()
+
+	fmt.Println(test.wasRun)
+	testcase.run(test)
+	fmt.Println(test.wasRun)
 }
