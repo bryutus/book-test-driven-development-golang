6e53e62312c42c4d58c0824c7a990e2834b7b71e
bryutus <bryutus@gmail.com>
2017-12-14 23:44:54 +0900

    :green_heart: setUpメソッドのテストを追加

--- part2/chapter19/xunit_test.go
+++ part2/chapter19/xunit_test.go
@@ -18,3 +18,14 @@ func TestRunning(testing *testing.T) {
 		testing.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestSetUp(testing *testing.T) {
+	testcase := newTestCase("TestMethod")
+	test := newWasRun()
+
+	testcase.run(test)
+
+	if got, want := test.wasSetUp, true; got != want {
+		testing.Errorf("got %v want %v", got, want)
+	}
+}
