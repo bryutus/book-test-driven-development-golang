7c9a352c9f91dd6e8f39b13c0abbc96b47fabc1f
bryutus <bryutus@gmail.com>
2017-12-14 23:53:27 +0900

    :+1: TestRunningをシンプルにするために実行前にwasRunのチェックをなくした
    TestSetUpがあるためなくすことが可能

--- part2/chapter19/xunit_test.go
+++ part2/chapter19/xunit_test.go
@@ -8,10 +8,6 @@ func TestRunning(testing *testing.T) {
 	testcase := newTestCase("TestMethod")
 	test := newWasRun()
 
-	if got, want := test.wasRun, false; got != want {
-		testing.Errorf("got %v want %v", got, want)
-	}
-
 	testcase.run(test)
 
 	if got, want := test.wasRun, true; got != want {
