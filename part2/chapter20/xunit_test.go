5da0dd4ca0f02cf8ba0f4ac19c05ed1d0daa8a41
bryutus <bryutus@gmail.com>
2017-12-15 00:35:13 +0900

    :green_heart: TestSetUpメソッドの名前を変更
    TestRunningメソッドが不要になったので削除

--- part2/chapter20/xunit_test.go
+++ part2/chapter20/xunit_test.go
@@ -4,18 +4,7 @@ import (
 	"testing"
 )
 
-func TestRunning(testing *testing.T) {
-	testcase := newTestCase("TestMethod")
-	test := newWasRun()
-
-	testcase.run(test)
-
-	if got, want := test.wasRun, true; got != want {
-		testing.Errorf("got %v want %v", got, want)
-	}
-}
-
-func TestSetUp(testing *testing.T) {
+func TestTemplateMethod(testing *testing.T) {
 	testcase := newTestCase("TestMethod")
 	test := newWasRun()
 
