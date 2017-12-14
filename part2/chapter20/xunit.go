7c838e8213628185cb7bda569a87d023178ed608
bryutus <bryutus@gmail.com>
2017-12-15 00:27:55 +0900

    :sparkles: chapter20 initial commit

---
+++ part2/chapter20/xunit.go
@@ -0,0 +1,38 @@
+package chapter20
+
+import (
+	"reflect"
+)
+
+type TestCase struct {
+	name string
+}
+
+func newTestCase(name string) *TestCase {
+	return &TestCase{name: name}
+}
+
+func (t *TestCase) run(w *WasRun) {
+	w.setUp()
+	method := t.name
+	fv := reflect.ValueOf(w).MethodByName(method)
+	fv.Call(nil)
+}
+
+type WasRun struct {
+	wasRun   bool
+	wasSetUp bool
+}
+
+func newWasRun() *WasRun {
+	return &WasRun{}
+}
+
+func (w *WasRun) setUp() {
+	w.wasRun = false
+	w.wasSetUp = true
+}
+
+func (w *WasRun) TestMethod() {
+	w.wasRun = true
+}
---
+++ part2/chapter20/xunit_test.go
@@ -0,0 +1,27 @@
+package chapter20
+
+import (
+	"testing"
+)
+
+func TestRunning(testing *testing.T) {
+	testcase := newTestCase("TestMethod")
+	test := newWasRun()
+
+	testcase.run(test)
+
+	if got, want := test.wasRun, true; got != want {
+		testing.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSetUp(testing *testing.T) {
+	testcase := newTestCase("TestMethod")
+	test := newWasRun()
+
+	testcase.run(test)
+
+	if got, want := test.wasSetUp, true; got != want {
+		testing.Errorf("got %v want %v", got, want)
+	}
+}
