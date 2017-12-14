b9ef250c2ac89f9510bb1d135e8e0bddeb6b7ab4
bryutus <bryutus@gmail.com>
2017-12-14 23:37:29 +0900

    :sparkles: chapter19 initial commit

---
+++ part2/chapter19/xunit_test.go
@@ -0,0 +1,20 @@
+package chapter19
+
+import (
+	"testing"
+)
+
+func TestRunning(testing *testing.T) {
+	testcase := newTestCase("TestMethod")
+	test := newWasRun()
+
+	if got, want := test.wasRun, false; got != want {
+		testing.Errorf("got %v want %v", got, want)
+	}
+
+	testcase.run(test)
+
+	if got, want := test.wasRun, true; got != want {
+		testing.Errorf("got %v want %v", got, want)
+	}
+}
