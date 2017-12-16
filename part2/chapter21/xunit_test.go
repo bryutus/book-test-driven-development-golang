b8f692531871c7f98cc3937728615bb71035504f
bryutus <bryutus@gmail.com>
2017-12-16 16:35:04 +0900

    :green_heart: 実行結果をサマリーするテストを追加

--- part2/chapter21/xunit_test.go
+++ part2/chapter21/xunit_test.go
@@ -14,3 +14,14 @@ func TestTemplateMethod(testing *testing.T) {
 		testing.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestSummary(testing *testing.T) {
+	testcase := newTestCase("TestMethod")
+	test := newWasRun()
+
+	result := testcase.run(test)
+
+	if got, want := result.summary(), "1 run, 0 failed"; got != want {
+		testing.Errorf("got %v want %v", got, want)
+	}
+}
