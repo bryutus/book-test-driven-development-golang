8f74a3c4e95a63b3dc3319e1f4bbf67ef5c8f9e5
bryutus <bryutus@gmail.com>
2017-12-16 16:35:55 +0900

    :sparkles: テスト結果を出力するsummaryメソッドを追加

--- part2/chapter21/xunit.go
+++ part2/chapter21/xunit.go
@@ -1,8 +1,22 @@
 package chapter21
 
 import (
+	"fmt"
 	"reflect"
 )
+
+type TestResult struct {
+	runCount int
+}
+
+func newTestResult() *TestResult {
+	return &TestResult{runCount: 1}
+
+}
+
+func (t *TestResult) summary() string {
+	return fmt.Sprintf("%v run, 0 failed", t.runCount)
+}
 
 type TestCase struct {
 	name string
@@ -12,12 +26,13 @@ func newTestCase(name string) *TestCase {
 	return &TestCase{name: name}
 }
 
-func (t *TestCase) run(w *WasRun) {
+func (t *TestCase) run(w *WasRun) *TestResult {
 	w.setUp()
 	method := t.name
 	fv := reflect.ValueOf(w).MethodByName(method)
 	fv.Call(nil)
 	w.tearDown()
+	return newTestResult()
 }
 
 type WasRun struct {
