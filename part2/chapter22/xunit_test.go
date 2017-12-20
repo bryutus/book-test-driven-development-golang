5c2176ae17d22314d95d0160ff5e21f1b04219ed
bryutus <bryutus@gmail.com>
2017-12-20 13:30:43 +0900

    :green_heart: テスト失敗時に失敗数をカウントしているかのテスト追加

--- part2/chapter22/xunit_test.go
+++ part2/chapter22/xunit_test.go
@@ -26,6 +26,17 @@ func TestSummary(testing *testing.T) {
 	}
 }
 
+func TestFailedResultFormatting(testing *testing.T) {
+	result := newTestResult()
+
+	result.testStarted()
+	result.testFailed()
+
+	if got, want := result.summary(), "1 run, 1 failed"; got != want {
+		testing.Errorf("got %v want %v", got, want)
+	}
+}
+
 /*
 func TestFailedSummary(testing *testing.T) {
 	testcase := newTestCase("TestBrokenMethod")
